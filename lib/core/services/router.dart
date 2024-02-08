2d2762ac2090b4dcf0567e1ca9bf58c69ef46f76
JabalnurIT <jabalnur.it@gmail.com>
2024-02-08 20:25:08 +0700

    git init

--- lib/core/services/router.dart
+++ lib/core/services/router.dart
@@ -1,14 +1,10 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pamelo_fruit_scan_app_fluttet_cnn/core/common/views/splash_screen.dart';
-import 'package:pamelo_fruit_scan_app_fluttet_cnn/core/common/widgets/main_text.dart';
-import 'package:pamelo_fruit_scan_app_fluttet_cnn/core/res/media_res.dart';
 
 import '../../src/scanner/presentation/bloc/scanner_bloc.dart';
 import '../../src/scanner/presentation/view/home_screen.dart';
 import '../common/views/page_under_construction.dart';
-import '../res/colours.dart';
 import 'injection_container.dart';
 
 part 'router.main.dart';
